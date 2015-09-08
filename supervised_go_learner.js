58156484733dba9a000f6dd22a852a9b69b9fbde
Max <max@recoset.com>
2015-09-07 23:47:56 -0400

    wgo.js to wgo and scratch pad html

--- supervised_go_learner.js
+++ supervised_go_learner.js
@@ -3,6 +3,14 @@
 var BOARD_SIZE = 19*19;
 
 var data_from_sgf = function(filename) {
+	if (filename === null) {
+		filename = "games/Pro10-1964-1.sgf";
+	}
+
+	fr = new FileReader();
+	fr.readAsText(filename);
+
+	return fr;
 	// load up an SGF file
 	// initialize a Board object (or player?) from WGO
 
@@ -31,14 +39,17 @@ var get_onehot = function(x, y){
 	var output = Array.apply(null, Array(BOARD_SIZE)).map(Number.prototype.valueOf,0);
 }
 
-layer_defs = [];
-layer_defs.push({type:'input', out_sx:19, out_sy:19});
-layer_defs.push({type:'conv', sx:5, filters:128, stride:1, pad:0, activation:'relu'});
-layer_defs.push({type:'conv', sx:3, filters:16, stride:1, pad:2, activation:'relu'});
+var get_trainer = function(){
+	layer_defs = [];
+	layer_defs.push({type:'input', out_sx:19, out_sy:19});
+	layer_defs.push({type:'conv', sx:5, filters:128, stride:1, pad:0, activation:'relu'});
+	layer_defs.push({type:'conv', sx:3, filters:16, stride:1, pad:2, activation:'relu'});
 
-layer_defs.push({type:'softmax', num_classes:BOARD_SIZE});
+	layer_defs.push({type:'softmax', num_classes:BOARD_SIZE});
 
-net = new convnetjs.Net();
-net.makeLayers(layer_defs);
+	net = new convnetjs.Net();
+	net.makeLayers(layer_defs);
 
-trainer = new convnetjs.SGDTrainer(net, {method:'adadelta', batch_size:32, l2_decay:0.001});
+	trainer = new convnetjs.SGDTrainer(net, {method:'adadelta', batch_size:32, l2_decay:0.001});
+	return trainer;	
+}
